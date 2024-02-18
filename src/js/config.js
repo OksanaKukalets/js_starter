4e833b197e6f9acbe461368dcaad872925a7ab36
Oksana Kukalets <oksana.kukalets@gmail.com>
2024-02-18 16:21:45 +0200

    init

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3b83def7-dc68-426d-9a86-59d34dbf81a9';
+export const MODAL_CLOSE_SEC = 2.5;
